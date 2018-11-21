c68cb1bee77cde5157ada6c542b01d6fefad9bd0
朱翔翔 <soso_hb_zx@163.com>
2018-11-21 16:48:02 +0800

    [feature]add

--- src/APP2.jsx
+++ src/APP2.jsx
@@ -1,99 +1,100 @@
 import React, { Component } from 'react';
-import {observable } from 'mobx';
-import {observer,inject} from 'mobx-react';
-import {socketConnect} from 'socket.io-react';
+import { observable } from 'mobx';
+import { observer, inject } from 'mobx-react';
+import { socketConnect } from 'socket.io-react';
 import Child from '@/Child';
 import px2vw from 'utils/px2vw.scss';
 import styles from './app2.scss';
-import {Upload,Button,Input} from 'antd'
-import {testable,timeout} from './utils/decorator';
+import { Upload, Button, Input } from 'antd'
+import { testable, timeout } from './utils/decorator';
 import MyStore from './store/my/myStore';
 import _ from 'lodash';
-import {Debounce} from 'lodash-decorators'
+import { Debounce } from 'lodash-decorators'
+import Loading from 'react-loading-spinkit';
 
 @testable
 @inject("homeStore")
 @observer
-export default class APP2 extends Component{
-    @observable dataArr =
-    [{name:'不可思议的CSSHHH',showLine:false} , {name: '导航栏',showLine:false} , {name:'光标小划线跟随',showLine:false}, {name:'PURE CSS',showLine:false} , {name:'NAV underline',showLine:false}]
+export default class APP2 extends Component {
+  @observable dataArr =
+    [{ name: '不可思议的CSSHHH', showLine: false }, { name: '导航栏', showLine: false }, { name: '光标小划线跟随', showLine: false }, { name: 'PURE CSS', showLine: false }, { name: 'NAV underline', showLine: false }]
 
-    constructor(props){
-      super(props);
-      this.state={
-          data:[{name:'不可思议的CSS',showLine:false} , {name: '导航栏',showLine:false} , {name:'光标小划线跟随',showLine:false}, {name:'PURE CSS',showLine:false} , {name:'NAV underline',showLine:false}]
-      };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [{ name: '不可思议的CSS', showLine: false }, { name: '导航栏', showLine: false }, { name: '光标小划线跟随', showLine: false }, { name: 'PURE CSS', showLine: false }, { name: 'NAV underline', showLine: false }]
+    };
+  }
 
-    componentDidMount(){
-      // this.props.socket.on('message',(msg)=>console.log(msg))
-    }
+  componentDidMount() {
+    // this.props.socket.on('message',(msg)=>console.log(msg))
+  }
 
 
-    test=(Index)=>{
-      for (let index = 0; index < this.dataArr.length; index++) {
-        if(index === Index){
-          this.dataArr.splice(index,1,{...this.dataArr[index],...{showLine:true}})
-        }else {
-            this.dataArr.splice(index,1,{...this.dataArr[index],...{showLine:false}})
-        }
+  test = (Index) => {
+    for (let index = 0; index < this.dataArr.length; index++) {
+      if (index === Index) {
+        this.dataArr.splice(index, 1, { ...this.dataArr[index], ...{ showLine: true } })
+      } else {
+        this.dataArr.splice(index, 1, { ...this.dataArr[index], ...{ showLine: false } })
       }
     }
+  }
 
-    _beforeUpload=(file)=>{
-        // var url = window.URL.createObjectURL(file);
-        // var img  = document.createElement("img");
-        // img.src = url;
-        // img.width = 60;
-        // img.height = 60;
-        // img.onload=(e)=>{
-        //   window.URL.revokeObjectURL(url)
-        // }
+  _beforeUpload = (file) => {
+    // var url = window.URL.createObjectURL(file);
+    // var img  = document.createElement("img");
+    // img.src = url;
+    // img.width = 60;
+    // img.height = 60;
+    // img.onload=(e)=>{
+    //   window.URL.revokeObjectURL(url)
+    // }
 
-       var div = document.getElementById('pic111');
-        var aLink = document.createElement('a');
-        var blob = new Blob([file]);
-        var evt = document.createEvent("HTMLEvents");
-        evt.initEvent("click", true, false);//initEvent 不加后两个参数在FF下会报错, 感谢 Barret Lee 的反馈
-        aLink.download = 'test3.jpg';
-        aLink.href = URL.createObjectURL(file);
-        aLink.dispatchEvent(evt);
-        aLink.innerHTML = '点击试试';
+    var div = document.getElementById('pic111');
+    var aLink = document.createElement('a');
+    var blob = new Blob([file]);
+    var evt = document.createEvent("HTMLEvents");
+    evt.initEvent("click", true, false);//initEvent 不加后两个参数在FF下会报错, 感谢 Barret Lee 的反馈
+    aLink.download = 'test3.jpg';
+    aLink.href = URL.createObjectURL(file);
+    aLink.dispatchEvent(evt);
+    aLink.innerHTML = '点击试试';
 
-        div.appendChild(aLink);
-      return false;
-    }
+    div.appendChild(aLink);
+    return false;
+  }
 
-    @timeout(3000)
-    clickItem(){
-      console.log('zzz')
-    }
-  
-    @Debounce(1000)
-    inputChange(){
-      // this.sayGood();
-      console.log('hahahahahahaha');
-    }
+  @timeout(3000)
+  clickItem() {
+    console.log('zzz')
+  }
 
-    render() {
-      
-      return (
-        <div className="con">
-              <div className="container">
-                  {this.dataArr.map( (item, index)=> 
-                    <span key={index} className= {item.showLine ? "tabItemCon2" : "tabItemCon"} onClick={()=>this.test(index)}>{item.name}</span>
-                  )
-                  }
-                  <Child/>
-                  <span>{this.props.homeStore.name}</span>
-              </div>
-                <Upload beforeUpload={this._beforeUpload}>
-                    <Button>点击选择图片</Button>
-                </Upload>
-                <div className="pic" id = 'pic111'>
+  @Debounce(1000)
+  inputChange() {
+    // this.sayGood();
+    console.log('hahahahahahaha');
+  }
 
-                </div>
-                <div className="test">
+  render() {
+
+    return (
+      <div className={styles.con}>
+        <div className={styles.container}>
+          <span>11111111</span>
+            {this.dataArr.map((item, index) =>
+              <span key={index} className={item.showLine ? styles.tabItemCon2 : styles.tabItemCon} onClick={() => this.test(index)}>{item.name}</span>
+            )
+            }
+          <span>{this.props.homeStore.name}</span>
+        </div>
+        <Upload beforeUpload={this._beforeUpload}>
+          <Button>点击选择图片</Button>
+        </Upload>
+        <div className={styles.pic} id='pic111'>
+
+        </div>
+        {/* <div className="test">
                   <div className={styles.itemCon} onClick={this.clickItem.bind(this)}>
                       <img src={require('./d.jpg')} alt="" className={styles.img}/>
                       <div className={styles.content}>
@@ -107,14 +108,41 @@ export default class APP2 extends Component{
                           <span className={styles.workNum}>{'admin'}</span>
                       </div>
                   </div>
-                </div>
-                <Input
-                  placeholder="请输入"
-                  onChange={this.inputChange.bind(this)}
-                  style={{height:40,marginTop:10}}
-                />
+                </div> */}
+        <Input
+          placeholder="请输入111"
+          onChange={this.inputChange.bind(this)}
+          className={styles.input}
+        />
+        <div className={styles.input2Con}>
+          <input type="text" placeholder="hahaha" className={styles.input2}/>
         </div>
-              )
-    };
+       
+        {/* <div className={styles.center}>
+                  <Loading name="line-scale"
+                           fadeIn="none"
+                           show={true}
+                           />
+                </div> */}
+        <div className={styles.left}>
+          <div className={styles.leftText}>
+            <div className={styles.borderStyle}>
+              <span>哈哈哈</span>
+              <div className={styles.leftHideContent}>
+                  <span>隐藏的内容</span>
+              </div>
+            </div>
+            <div className={styles.leftUnder}>
+              <span className={styles.item}>11111111</span>
+              <span className={styles.item}>舒服舒服</span>
+              <span className={styles.item}>舒服舒服</span>
+              <span className={styles.item}>沮丧</span>
+              <span className={styles.item}>沮丧卡咖啡给对方</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  };
 
-    }
+}
---
+++ src/pages/testPage/demo1.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './demo1.scss';
+export default class Demo1 extends React.Component {
+
+    render() {
+        return (
+            <div className={styles.container}>
+                <span>hahahaha</span>
+            </div>
+        )
+    }
+}
