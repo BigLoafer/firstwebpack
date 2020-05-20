4d7c523f0069ec277590c6bfa8c43d364f608e43
zxx <soso_hb_zx@163.com>
2020-05-20 10:29:43 +0800

    1

--- src/pages/testPage/demo1.jsx
+++ src/pages/testPage/demo1.jsx
@@ -1,12 +1,35 @@
-import React from 'react';
-import styles from './demo1.scss';
-export default class Demo1 extends React.Component {
+import React from "react";
+import styles from "./demo1.scss";
 
-    render() {
-        return (
-            <div className={styles.container}>
-                <span>hahahaha</span>
-            </div>
-        )
+function debounce(fn, ms) {
+  let timer;
+  console.log(arguments);
+  return function(...args) {
+    console.log(args);
+    if (timer) {
+      clearTimeout(timer);
     }
+    timer = setTimeout(() => {
+      fn(...args);
+      timer = null;
+    }, ms);
+  };
+}
+
+export default class Demo1 extends React.Component {
+  test = str => {
+    console.log(`${str}`);
+  };
+
+  test2 = debounce(this.test, 1000);
+
+  render() {
+    return (
+      <div className={styles.container}>
+        <span onClick={() => this.test("llllkhhddgd")}>hahahaha</span>
+        <br />
+        <span onClick={() => this.test2("hdhgdgggdg")}>hahahaha</span>
+      </div>
+    );
+  }
 }
